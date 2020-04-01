35355d2b5a041bfc2a579cb531775bc46ec8cfc3
Richard Blake <ricky@Richards-MacBook-Pro.local>
2020-04-01 12:56:17 -0500

    added files

---
+++ script.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+
+}
+
+function randomizeColors() {
+	color1.value = '#' + (function co(lor){   return (lor +=
+  [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+  && (lor.length == 6) ?  lor : co(lor); })('');
+	color2.value = '#' + (function co(lor){   return (lor +=
+  [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+  && (lor.length == 6) ?  lor : co(lor); })('');
+
+  setGradient();
+
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomizeColors);
